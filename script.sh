9aba18e6e4e07dae5b7ecce2987a6cc6a116a289
eliezersamuel <eliezer_sss@hotmail.com>
2024-07-10 21:36:16 -0300

    feat: bd added

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # script.sh
-# Versao: 0.0.1
+# Versao: 0.1.0
 # Script para: Troca do /etc/hosts
 # 2024-07-03 13:51-0300
 # Codificacao utf-8
 # Autor: Eliezer Samuel
 
 # Testa se tem o dialog, senão instala em background
-[ ! -x "$(which dialog)" ] && sudo apt install dialog 1> /dev/null 2>&1
+[ ! -x "$(which dialog)" ] && sudo apt install dialog -y 1> /dev/null 2>&1
 
 # Verifica se o script está sendo executado como root
 if [ "$EUID" -ne 0 ]; then
@@ -15,12 +15,23 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+# Verifica o local "pwd" onde está o script
+LOCAL=$(dirname "$(readlink -f "$0")")
+
+# Verifica se tem o bd.txt
+if [ -e "$LOCAL/bd.txt" ]; then
+    BD=$(cat "$LOCAL/bd.txt")
+fi
+
 # Função para copiar o arquivo selecionado para /etc/hosts
 trocar_hosts() {
     local arquivo=$1
-    sudo cp "./hosts/$arquivo" /etc/hosts
+    sudo cp "$LOCAL/hosts/$arquivo" /etc/hosts
     if [ $? -eq 0 ]; then
         echo "Arquivo /etc/hosts substituído com sucesso."
+        if [ -e "$LOCAL/bd.txt" ]; then
+            echo $arquivo > $LOCAL/bd.txt;
+        fi
     else
         echo "Erro ao substituir o arquivo /etc/hosts."
         exit 1
@@ -28,7 +39,7 @@ trocar_hosts() {
 }
 
 # Listar arquivos no diretório ./hosts/
-HOSTS=$(ls ./hosts/)
+HOSTS=$(ls $LOCAL/hosts/)
 
 # Verifica se encontrou arquivos no diretório ./hosts/
 if [ -z "$HOSTS" ]; then
@@ -45,7 +56,7 @@ done
 # Cria a lista de opções para o radiolist
 OPTIONS=()
 for FILE in $HOSTS; do
-    OPTIONS+=("$FILE" "$FILE" off)
+    OPTIONS+=("$FILE" "" off)
 done
 
 # Verifica se passou argumentos e se não apresenta um dialog para escolher o host especifico
@@ -54,12 +65,25 @@ if [ "$#" -ge 1 ]; then
     if [ "$(echo "$result" | tr '[:upper:]' '[:lower:]')" = "help" ]; then
         echo "Escolha uma das opcoes abaixo:"
         for i in "${OPTIONSLIST[@]}"; do
-            echo "- $i"
+            tput bold;
+            if [ -n "$BD" ]; then
+                if [ "$i" = "$BD" ]; then
+                    tput setaf 2;  # Green text color
+                    echo "* $i"
+                else
+                    tput setaf 1;  # Red text color
+                    echo "- $i"
+                fi
+            else
+                tput setaf 4;  # Blue text color
+                echo "- $i"
+            fi
+            tput sgr0;  # Reset text color
         done
         exit 1
     fi
 
-    if [ -e "./hosts/$result" ]; then
+    if [ -e "$LOCAL/hosts/$result" ]; then
         echo "Você selecionou: $result"
     else
         echo "Erro: O arquivo '$result' não existe no diretório ./hosts/"
@@ -81,4 +105,8 @@ else
 fi
 
 # Chama a função para copiar o arquivo selecionado
-trocar_hosts "$result"
+if [ -n "$result" ]; then
+    trocar_hosts "$result"
+else    
+    exit 1
+fi
